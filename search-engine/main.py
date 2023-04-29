e165e0dff920325effe71f37f8092095bed41d57
FZapata <97852383+felipezapata01@users.noreply.github.com>
2023-04-28 23:44:17 -0500

    Add Templates to main

--- search-engine/main.py
+++ search-engine/main.py
@@ -1,15 +1,13 @@
-from typing import Union
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/", response_class=HTMLResponse)
+async def hello_world(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
