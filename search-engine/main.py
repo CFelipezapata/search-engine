24a7ffe519b421b2b1f80c85f1ba6d1a5872376b
FZapata <97852383+felipezapata01@users.noreply.github.com>
2023-05-03 23:42:26 -0500

    Import additional dependencies

--- search-engine/main.py
+++ search-engine/main.py
@@ -3,6 +3,11 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
+from bs4 import BeautifulSoup
+import requests
+
+
+
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
